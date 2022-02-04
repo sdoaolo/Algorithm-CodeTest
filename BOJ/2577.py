a3c22bc1d21b9f608eb5273afab548470a85dc86
KangJiEun <48430781+sdoaolo@users.noreply.github.com>
2022-02-04 18:32:36 +0900

    2577_숫자의 개수

---
+++ BOJ/2577.py
@@ -0,0 +1,13 @@
+#2577 숫자의 개수
+import sys
+
+res = 1
+for _ in range(3):
+    a =  int(sys.stdin.readline().strip())
+    res *= a
+    
+res = str(res)
+for i in range(10):
+    print("{}".format(res.count(str(i))))
+    
+        
