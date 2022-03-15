715f92ae8ecb00b46d0efad84e350269d206f64a
KangJiEun <48430781+sdoaolo@users.noreply.github.com>
2022-03-15 20:00:57 +0900

    11728_배열 합치기

---
+++ BOJ/silver/11728.py
@@ -0,0 +1,13 @@
+#11728_배열 합치기
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+li = sorted(a+b)
+
+for i in li:
+    print(i, end=" ")
