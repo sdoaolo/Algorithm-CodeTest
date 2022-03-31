68697195053ace01382d2864906dd5fdd7a8dd0b
KangJiEun <48430781+sdoaolo@users.noreply.github.com>
2022-03-31 23:56:04 +0900

    14627_파닭파닭_binary Search_silver3

---
+++ BOJ/silver/14627.py
@@ -0,0 +1,30 @@
+#14627_파닭파닭_binary Search_silver3
+
+import sys
+input = sys.stdin.readline
+
+s, c = map(int,input().split())
+L = [int(input()) for _ in range(s)]
+
+start = 0
+end = 1000000000
+
+resChick = 0
+
+while(start<=end):
+    mid = (start+end) // 2
+    if mid == 0:
+        mid = 1
+        
+    cnt = sum(pa//mid for pa in L)
+    if cnt >= c: #cnt 가 같다 == mid 저장,
+        # cnt의 개수가 크다는 것은 mid를 더크게 해도 된다는 것. 
+        resChick = max(resChick,mid)
+        start = mid+1
+    else:
+        end = mid -1
+ 
+ # 하나의 파닭에는 하나 이상의 파가 들어가면 안 된다.
+ # 전체 파의 합  - (파닭의 개수 * 치킨에 들어가는 파)      
+res = sum(L) - (c * resChick)
+print(res)
