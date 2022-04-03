e106771f062580fcbb270b7a63d0ab8f33beeeda
KangJiEun <48430781+sdoaolo@users.noreply.github.com>
2022-04-04 00:42:32 +0900

    14627_파닭파닭_binary Search_silver3

--- BOJ/silver/14627.py
+++ BOJ/silver/14627.py
@@ -6,16 +6,14 @@ input = sys.stdin.readline
 s, c = map(int,input().split())
 L = [int(input()) for _ in range(s)]
 
-start = 0
-end = 1000000000
+start = 1
+end = max(L)
 
 resChick = 0
 
 while(start<=end):
     mid = (start+end) // 2
-    if mid == 0:
-        mid = 1
-        
+    
     cnt = sum(pa//mid for pa in L)
     if cnt >= c: #cnt 가 같다 == mid 저장,
         # cnt의 개수가 크다는 것은 mid를 더크게 해도 된다는 것. 
