db190ea520379578207d18dd4777322b4c54ee28
KangJiEun <48430781+sdoaolo@users.noreply.github.com>
2022-04-05 16:28:59 +0900

    7562_나이트의 이동_bfs_silver1

---
+++ BOJ/silver/7562.py
@@ -0,0 +1,46 @@
+#7562_나이트의 이동_dfs/bfs_silver1
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+#dr dc 설정확실하게 하자!!
+#dr = [-2,-1, 1, 2, -1, -1, 1  ,2]
+#dc = [ 1 ,2, 2, 1, -1, -2,-2, -1]
+
+dr = [-1, -2, -2, -1, 1, 2, 2, 1]
+dc = [2, 1, -1, -2, -2, -1, 1, 2]
+
+def bfs(y,x,gr,gc):
+    queue = deque()
+    queue.append([y,x])
+    visited[y][x] = 1
+    
+    while queue:
+        r,c = queue.popleft()
+        
+        if r == gr and c == gc: #방문하고자 하는 곳이면
+            print(visited[r][c] -1)
+            return
+                
+        for i in range(8):
+            rr = r+dr[i]
+            cc= c+dc[i]
+            
+            if (0<=rr<l) and(0<=cc<l) and not visited[rr][cc]:
+                queue.append([rr,cc])
+                visited[rr][cc] = visited[r][c] +1
+                
+t = int(input())
+for _ in range(t):
+    l = int(input())
+    nr, nc  = map(int,input().split())
+    gr, gc = map(int,input().split())
+    
+    if nr == gr and nc == gc:
+        print(0)
+        continue
+    
+    visited =[[False]*l for _ in range(l)]
+    bfs(nr,nc,gr,gc)
+    
+ 
